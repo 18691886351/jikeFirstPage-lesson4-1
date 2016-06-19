a1aab28d7e05567a42ca956974875f8d63996e1f
qianl <450374167@qq.com>
2016-06-19 21:59:25 +0800

    完成了一部分，还差一些“

--- static/js/index.js
+++ static/js/index.js
@@ -22,17 +22,32 @@ $(document).ready(function() {
     });
 
     /*
-    点击pagination中的span，滚动到指定的页面。
-    1.switch1,对应滚动面板2
-	  switch2,对应滚动面板3
-	  以此类推
-	2.改变自己的background为绿色，并还原其他的switch背景颜色为白色
+    .index-banner content的滚动。
+     0 1 2 3 4 5 6 7
+    0 1 2 3 4 5 6 7 8
+    0=0
+    1=-560
+    2=-1120
+    3=-1680
+    4=-2240
+    5=-2800
+    6=-3360
+    7=-3920
+    其中0,6相同,1,7相同
+    为了在视觉上实现循环滚动的效果。
+    页面加载时，从1开始加载
     */
-
    	var offer = 560;
-    var max = -4480+560;
-    var cur = -560;
-
+    var max = 4480-560;
+    var cur =0;
+    /*
+    点击pagination中的span，滚动到指定的页面。
+    1.switch0,对应滚动面板1
+      switch1,对应滚动面板2
+      ....
+      switch5,对应滚动面板6
+    2.改变自己的background为绿色，并还原其他的switch背景颜色为白色
+    */
     $(".lesson-center-focus .index-banner .pagination .switch").each(function(i){
     	$(this).click(function(){
     		/*将自己的背景改变*/
@@ -43,7 +58,7 @@ $(document).ready(function() {
 			cur=(i+1)*-1*offer;
     		/*alert(_cur);*/
     		/*$(".lesson-center-focus .index-banner .content").css({"transform":"translate3d(" + _cur + "px, 0px, 0px)","transition":"all 1s ease-in"});*/
-    		changeContent();
+    		rollContent();
     	});
     });
      
@@ -55,50 +70,75 @@ $(document).ready(function() {
     	
     }
 
-    /*
-    1.对lesson-center-focus的content自动进行滚屏
-    2.根据_cur的值,来对应switch。
-    */
-    setInterval(circulation,100000);
+   
 
-    function circulation(){
-    	changeContent();
-    	cur = cur - offer;
+    /*
+    对lesson-center-focus的content自动进行滚屏
+    自动循环滚动，content水平向左移动。视觉上图片向右移动。
+    */
+    setInterval(autoCirculation,100000);
+    function autoCirculation(){
+        cur = cur - offer;
+
+    	rollContent();
     }
 
-    function changeContent() {
+    /*
+    1.switch与面板同步
 
     
-    	/*对应到相应的switch*/
-    	var _switchIdx=Math.abs(cur/offer);
+    */
+    function rollContent() {
+    	
+        var _edge=0;/*0非边缘，1边缘*/
+        if(cur<(max*-1)){
+            cur =-560;
+            _edge=1;
+        }
+        if(cur>0){
+             cur =-1*(6*offer);
+             _edge=1
+        }
+        /*0-7*/
+    	var _panel_idx=Math.abs(cur/offer)%8;
+
     	var _obj;
+        var _cur;
 
-		if(_switchIdx>=7){/*视觉循环*/
-    		_obj=$(".lesson-center-focus .index-banner .pagination .switch").eq(0);
-    		changeSwitch(_obj);
-    	}
-    	else{
-    		_obj=$(".lesson-center-focus .index-banner .pagination .switch").eq(_switchIdx-1);
-    		changeSwitch(_obj);
-    	}
-   		
+        if(_panel_idx==7){
+            _obj=$(".lesson-center-focus .index-banner .pagination .switch").eq(1-1);
+        }else if(_panel_idx==0){
+            _obj=$(".lesson-center-focus .index-banner .pagination .switch").eq(6-1);
+        }
+        else{
+            _obj=$(".lesson-center-focus .index-banner .pagination .switch").eq(_panel_idx-1);
+        }
+        changeSwitch(_obj);
 
-        if (cur >= max) {
-            $(".lesson-center-focus .index-banner .content").css({"transform":"translate3d(" + cur + "px, 0px, 0px)","transition":"all 1s ease-in"});
-        }else {
-            cur=-560;
+        _cur=_panel_idx*(-1)*offer;
+        /*alert("cur="+cur+" _cur="+_cur+" _edge="+_edge+"_panel_idx="+_panel_idx);*/
+        if(_edge==1){
             $(".lesson-center-focus .index-banner .content").removeAttr("style");
-            $(".lesson-center-focus .index-banner .content").css("transform","translate3d(" + cur + "px, 0px, 0px)");
-        };
+            $(".lesson-center-focus .index-banner .content").css("transform","translate3d(" + _cur + "px, 0px, 0px)");
+        }else{
+            $(".lesson-center-focus .index-banner .content").css({"transform":"translate3d(" + _cur + "px, 0px, 0px)","transition":"all 1s ease-in"});
+        }
         
     }
 
     /*
-    1.点击lesson-center-focus左箭头与右箭头完成滚动
+    点击lesson-center-focus左箭头面板向左滚动
     */
     $("#banner-left").click(function(){
-    	changeContent();
-    	cur=cur+offer;
+        cur=cur+offer;
+    	rollContent();
+    });
+    /*
+    点击lession-center-focus右箭头面板向右滚动
+    */
+    $("#banner-right").click(function(){
+        cur=cur-offer;
+        rollContent();
     });
 
 });
